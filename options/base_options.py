16936d65bf7e783838eb40916fa4443a6d8a594e
AlexeyKrylov <47530556+AlexeyKrylov@users.noreply.github.com>
2022-01-09 19:09:24 +0300

    Add files via upload

--- options/base_options.py
+++ options/base_options.py
@@ -22,7 +22,7 @@ class BaseOptions():
         # basic parameters
         parser.add_argument('--name', type=str, default='testing_0.1',
                             help='name of the experiment. It decides where to store samples and models')
-        parser.add_argument('--dataroot', default='data/imgs/', help='path to images')
+        parser.add_argument('--dataroot', default='data/img_align_celeba/img_align_celeba/', help='path to images')
         parser.add_argument('--gpu_ids', type=str, default='0', help='gpu ids: e.g. 0  0,1,2, 0,2. use -1 for CPU')
         parser.add_argument('--checkpoints_dir', type=str, default='./checkpoints', help='models are saved here')
         parser.add_argument('--neptune_api_token', type=str,
@@ -36,9 +36,9 @@ class BaseOptions():
                             help='# of input image channels: 3 for RGB and 1 for grayscale')
         parser.add_argument('--output_nc', type=int, default=3,
                             help='# of output image channels: 3 for RGB and 1 for grayscale')
-        parser.add_argument('--ngf', type=int, default=2, help='# of gen filters in the last conv layer')
-        parser.add_argument('--ndf', type=int, default=2, help='# of discrim filters in the first conv layer')
-        parser.add_argument('--nz', type=int, default=128, help='# length of latent vector')
+        parser.add_argument('--ngf', type=int, default=8, help='# of gen filters in the last conv layer')
+        parser.add_argument('--ndf', type=int, default=8, help='# of discrim filters in the first conv layer')
+        parser.add_argument('--nz', type=int, default=10, help='# length of latent vector')
         parser.add_argument('--netD', type=str, default='celeba_discriminator',
                             help='specify discriminator architecture [mnist_discriminator, celeba_discriminator].')
         parser.add_argument('--netG', type=str, default='celeba_generator',
@@ -54,18 +54,18 @@ class BaseOptions():
         parser.add_argument('--init_gain', type=float, default=0.02,
                             help='scaling factor for normal, xavier and orthogonal.')
         # dataset parameters
-        parser.add_argument('--dataset_mode', type=str, default='rls',
+        parser.add_argument('--dataset_mode', type=str, default='celeba',
                             help='chooses how datasets are loaded. [celeba | mnist | rls]')
         parser.add_argument('--serial_batches', action='store_true',
                             help='if true, takes images in order to make batches, otherwise takes them randomly')
         parser.add_argument('--num_threads', default=4, type=int, help='# threads for loading data')
-        parser.add_argument('--batch_size', type=int, default=6, help='input batch size')
-        parser.add_argument('--load_size', type=int, default=512, help='scale images to this size')
-        parser.add_argument('--crop_size', type=int, default=512, help='then crop to this size')
-        parser.add_argument('--max_dataset_size', type=int, default=512,
+        parser.add_argument('--batch_size', type=int, default=32, help='input batch size')
+        parser.add_argument('--load_size', type=int, default=128, help='scale images to this size')
+        parser.add_argument('--crop_size', type=int, default=128, help='then crop to this size')
+        parser.add_argument('--max_dataset_size', type=int, default=32000,
                             help='Maximum number of samples allowed per dataset. If the dataset directory contains more than max_dataset_size, only a subset is loaded.')
-        parser.add_argument('--preprocess', type=str, default='randomcrop',
-                            help='scaling and cropping of images at load time [resize | centercrop | none | randomcrop]')
+        parser.add_argument('--preprocess', type=str, default='resize & centercrop',
+                            help='scaling and cropping of images at load time [resize | centercrop | none | randomcrop | rotate | horflip | verflip]')
         # additional parameters
         parser.add_argument('--epoch', type=str, default='latest',
                             help='which epoch to load? set to latest to use latest cached model')
